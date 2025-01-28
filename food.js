6a0e8cc3efffd784e5be1773ad066188a23260a5
MaxPog <pogrebniuc1996@gmail.com>
2025-01-28 19:39:04 +0200

    Add files via upload

---
+++ food.js
@@ -0,0 +1,31 @@
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid,js'
+
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 3
+
+export function update() {
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE)
+        food = getRandomFoodPosition()
+    }
+ 
+}
+
+export function draw() {
+    snakeBody.forEach(segment => {
+        const foodElement = document.createElement('div')
+        foodElement.style.gridRowStart = segment.y
+        foodElement.style.gridColumnStart = segment.x
+        foodElement.classList.add('snake')
+        gameBoard.appendChild(foodElement)
+    })
+}
+
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
+}
