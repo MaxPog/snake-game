8be5c38d2be096aa99babaf02e62bda7202f56b7
MaxPog <pogrebniuc1996@gmail.com>
2025-01-29 15:03:01 +0200

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,8 +1,7 @@
 import { getInputDirection } from "./input.js";
 
-export let snakeBody = [{ x: 11, y: 11 }];
-
 export const SNAKE_SPEED = 5
+const snakeBody = [{ x: 11, y: 11 }]
 let newSegments = 0
 const gameBoard = document.getElementById('game-board')
 
